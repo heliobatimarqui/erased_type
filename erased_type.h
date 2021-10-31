c4d528c2233f61baf5c0916ecd026ff2f75bac6e
Helio <helio.batimarqui@gmail.com>
2021-10-31 20:20:33 +0000

    Provides the make_erased_type method and fixes a few bugs.

--- erased_type.h
+++ erased_type.h
@@ -22,8 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-
-
 #ifndef ERASED_TYPE_ERASED_TYPE_H
 #define ERASED_TYPE_ERASED_TYPE_H
 
@@ -32,6 +30,8 @@ SOFTWARE.
 #include <cstdint>
 #include <memory>
 #include <exception>
+#include <stdexcept>
+#include <utility>
 
 template<bool cond, typename T, typename U>
 struct if_else_type {
@@ -55,14 +55,17 @@ public:
     }
 };
 
-template<size_t BUFFER_SIZE = 32>
+template<size_t BUFFER_SIZE>
 class erased_type {
     struct manager_base {
-        virtual const std::type_info& type() const = 0;
+        virtual bool can_T_be_memcopied() = 0;
         virtual void destroy_fn(erased_type&) = 0;
-        virtual void copy_fn(const erased_type&, erased_type&) = 0;
+        virtual void copy_fn(const erased_type& from, erased_type& to) = 0;
+        virtual void move_fn(erased_type& from, erased_type& to) = 0;
+        virtual const std::type_info& T_type_info() const = 0;
         virtual void* get_pointer_to_val(erased_type& et) = 0;
         virtual const void* get_pointer_to_val(const erased_type& et) = 0;
+
     };
 
     template<typename T>
@@ -70,7 +73,7 @@ class erased_type {
         template<typename ...Args>
         auto& create_fn(erased_type& et, Args&&... args) {
             auto aligned_ptr = static_cast<T*>(get_pointer_to_val(et));
-            new(aligned_ptr)T(std::forward<Args...>(args...));
+            new(aligned_ptr)T(std::forward<Args>(args)...);
             return *aligned_ptr;
         }
 
@@ -83,20 +86,32 @@ class erased_type {
             create_fn(to, val);
         }
 
+        void move_fn(erased_type& from, erased_type& to) override {
+            T* val_from = static_cast<T*>(get_pointer_to_val(from));
+            create_fn(to, std::move(*val_from));
+        }
+
         void* get_pointer_to_val(erased_type& et) override {
             const auto& as_const = et;
             return const_cast<void*>(get_pointer_to_val(as_const));
         }
 
         const void* get_pointer_to_val(const erased_type& et) override {
+            static_assert(sizeof(T) <= BUFFER_SIZE, "Buffer can't fit T.");
             auto space = BUFFER_SIZE;
             void* buffer = const_cast<std::decay_t<decltype(et.m_buffer)>>(et.m_buffer);
             auto aligned_ptr = std::align(alignof(T), sizeof(T), buffer, space);
+            if(aligned_ptr == nullptr)
+                throw std::runtime_error("Couldn't get aligned pointer to T");
             return aligned_ptr;
         }
 
-        const std::type_info& type() const override {
+        const std::type_info& T_type_info() const override {
             return typeid(T);
+        }
+
+        bool can_T_be_memcopied() override {
+            return std::is_trivially_copyable_v<T>;
         }
 
         static manager* instance() {
@@ -115,36 +130,43 @@ public:
 
     template<typename T, typename = std::enable_if_t<!std::is_same_v<std::decay_t<T>, erased_type>>>
     erased_type(T&& val) {
-        emplace<T>(std::forward<decltype(val)>(val));
+        emplace<std::decay_t<T>>(std::forward<decltype(val)>(val));
+    }
+
+    template<typename T, typename ...Args>
+    erased_type(std::in_place_type_t<T>, Args&&... args) {
+        emplace<T>(std::forward<Args>(args)...);
     }
 
     erased_type(const erased_type& other) {
-        if(other.has_value()) {
-            other.m_manager->copy_fn(other, *this);
-            m_manager = other.m_manager;
-        }
+        *this = other;
     }
 
     erased_type(erased_type&& other) noexcept {
-        swap_values(other);
-        other.reset();
+        *this = std::move(other);
     }
 
     ~erased_type() {
         reset();
     }
 
-    void swap_values(erased_type& other) noexcept {
-        auto temp_buffer = std::make_unique<std::byte[]>(BUFFER_SIZE);
-        std::memcpy(temp_buffer, m_buffer, buffer_size);
-        std::memcpy(m_buffer, other.m_buffer, buffer_size);
-        std::memcpy(other.m_buffer, temp_buffer, buffer_size);
-        std::swap(m_buffer, other.m_buffer);
+    void swap(erased_type& other) noexcept {
+        if ((!has_value() || m_manager->can_T_be_memcopied()) && (!other.has_value() || other.m_manager->can_T_be_memcopied())) {
+            std::byte temp_buffer[BUFFER_SIZE];
+            std::memcpy(temp_buffer, m_buffer, BUFFER_SIZE);
+            std::memcpy(m_buffer, other.m_buffer, BUFFER_SIZE);
+            std::memcpy(other.m_buffer, temp_buffer, BUFFER_SIZE);
+            std::swap(m_manager, other.m_manager);
+        }
+        else {
+            erased_type temp = std::move(*this);
+            *this = std::move(other);
+            other = std::move(temp);
+        }
     }
 
     template<typename T, typename ...Args>
     auto& emplace(Args&&... args) {
-        static_assert(BUFFER_SIZE >= sizeof(T), "Buffer size is smaller than type size.");
         reset();
         auto manager_ptr = manager<std::decay_t<T>>::instance();
         auto& val_ref = manager_ptr->create_fn(*this, std::forward<Args...>(args)...);
@@ -153,19 +175,19 @@ public:
     }
 
     void reset() {
-        if(has_value()) {
+        if(this->has_value()) {
             m_manager->destroy_fn(*this);
             m_manager = nullptr;
         }
     };
 
-    bool has_value() const {
+    bool has_value() const noexcept {
         return m_manager != nullptr;
     }
 
     const std::type_info& type() const noexcept {
-        if(m_manager != nullptr)
-            return m_manager->type();
+        if(this->has_value())
+            return m_manager->T_type_info();
         else
             return typeid(void);
     }
@@ -174,13 +196,34 @@ public:
         return BUFFER_SIZE;
     }
 
-    template<typename T>
-    std::decay_t<T>& operator=(T&& val) {
-        return emplace<T>(std::forward<T>(val));
+    template<typename T, typename = std::enable_if_t<!std::is_same_v<std::decay_t<T>, erased_type>, void>>
+    erased_type& operator=(T&& val) {
+        emplace<T>(std::forward<T>(val));
+        return *this;
     }
 
-};
+    erased_type& operator=(const erased_type& other) {
+        if(this != &other) {
+            this->reset();
+            if(other.has_value()) {
+                other.m_manager->copy_fn(other, *this);
+                m_manager = other.m_manager;
+            }
+        }
+        return *this;
+    }
 
+    erased_type& operator=(erased_type&& other) noexcept {
+        if(this != &other) {
+            this->reset();
+            if(other.has_value()) {
+                other.m_manager->move_fn(other, *this);
+                this->m_manager = other.m_manager;
+            }
+        }
+        return *this;
+    }
+};
 
 template<typename T, typename ET, typename = typename std::enable_if<std::is_same_v<std::decay_t<ET>, erased_type<std::decay_t<ET>::buffer_size()>>, void>::type, typename = typename std::enable_if<std::is_same_v<T, std::decay_t<T>>, void>::type>
 auto erased_type_cast(ET&& et) -> if_else_t<std::is_rvalue_reference_v<decltype(et)>, T, if_else_t<std::is_const_v<std::remove_reference_t<decltype(et)>>, const T&, T&>> {
@@ -196,6 +239,11 @@ auto erased_type_cast(ET&& et) -> if_else_t<std::is_rvalue_reference_v<decltype(
         return *static_cast<T*>(et.m_manager->get_pointer_to_val(et));
     }
 }
+
+template<typename T, typename ...Args>
+auto make_erased_type(Args&&... args) {
+    return erased_type<sizeof(T)>(std::forward<Args>(args)...);
+};
 
 
 #endif //ERASED_TYPE_ERASED_TYPE_H
